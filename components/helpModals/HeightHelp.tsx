5d7c51088fb7468a95d63df1cad94f7eb3c5206d
Piotr Sabatura <piotrsabatura@gmail.com>
2025-09-09 11:05:02 +0200

    changed name of one component

--- components/helpModals/HeightHelp.tsx
+++ components/helpModals/HeightHelp.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-const ProfileHeightHelp = () => {
+const HeightHelp = () => {
 
    
   return (
@@ -38,4 +38,4 @@ const ProfileHeightHelp = () => {
   )
 }
 
-export default ProfileHeightHelp
+export default HeightHelp
